c299d8e9792fe6e2161fa5da93462586cb38e461
Nishad Gothoskar <nishadg@mit.edu>
2022-10-13 03:26:29 +0000

    test scripts

---
+++ test/test_render_sphere.py
@@ -0,0 +1,35 @@
+import numpy as np
+import jax.numpy as jnp
+import jax
+from jax3dp3.rendering import render_sphere
+from jax3dp3.utils import (
+    make_centered_grid_enumeration_3d_points,
+    quaternion_to_rotation_matrix,
+    depth_to_coords_in_camera
+)
+from jax3dp3.shape import get_cube_shape
+from jax3dp3.viz.img import save_depth_image
+
+import matplotlib.pyplot as plt
+
+h, w, fx_fy, cx_cy = (
+    300,
+    300,
+    jnp.array([200.0, 200.0]),
+    jnp.array([150.0, 150.0]),
+)
+
+pose = jnp.array(
+    [
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 5.6],
+        [0.0, 0.0, 0.0, 1.0],
+    ]
+)
+
+gt_image = render_sphere(pose, 0.3, h,w, fx_fy, cx_cy)
+print('gt_image.shape ',gt_image.shape)
+from IPython import embed; embed()
+
+save_depth_image(gt_image[:,:,2], 5.0, "imgs/depth.png")
