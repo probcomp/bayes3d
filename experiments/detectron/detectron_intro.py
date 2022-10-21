7e42a45a85e3b0a05bc7f8b48e8d20a79b341b83
Nishad Gothoskar <nishadg@mit.edu>
2022-10-21 16:39:20 +0000

    detectron

---
+++ experiments/detectron/detectron_intro.py
@@ -0,0 +1,32 @@
+import detectron2
+from detectron2.utils.logger import setup_logger
+
+import numpy as np
+import os, json, cv2, random
+
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+im = cv2.imread("/home/ubuntu/data/test/000049/rgb/000001.png")
+
+cfg = get_cfg()
+# add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+cfg.merge_from_file(model_zoo.get_config_file("LVISv0.5-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_1x.yaml"))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("LVISv0.5-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_1x.yaml")
+
+predictor = DefaultPredictor(cfg)
+outputs = predictor(im)
+
+v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+
+cv2.imwrite("img.png", out.get_image()[:, :, ::-1])
+
+
+
+from IPython import embed; embed()
