627aa685e7b5f1a53287242a883e4c7f716ccb0f
Nishad Gothoskar <nishadg@mit.edu>
2022-10-30 19:13:54 +0000

    gaussian vmf sample and logpdf

--- jax3dp3/distributions.py
+++ jax3dp3/distributions.py
@@ -3,17 +3,29 @@ import jax
 import jax.numpy as jnp
 from .transforms_3d import (
     quaternion_to_rotation_matrix,
+    rotation_matrix_to_quaternion
 )
-tfd = tfp.distributions
 
-VonMisesFisher = tfd.VonMisesFisher
-
-def gaussian_vmf(key, var, conc):
-    translation  = jax.random.multivariate_normal(key, jnp.zeros(3), jnp.eye(3) * var)
-    v = VonMisesFisher(
-        jnp.array([1.0, 0.0, 0.0, 0.0]), conc
+def gaussian_vmf(key, var, concentration):
+    translation = tfp.distributions.MultivariateNormalDiag(jnp.zeros(3), jnp.ones(3) * var).sample(seed=key)
+    quat = tfp.distributions.VonMisesFisher(
+        jnp.array([1.0, 0.0, 0.0, 0.0]), concentration
     ).sample(seed=key)
-    rot_matrix =  quaternion_to_rotation_matrix(v)
+    rot_matrix =  quaternion_to_rotation_matrix(quat)
     return jnp.vstack(
         [jnp.hstack([rot_matrix, translation.reshape(3,1) ]), jnp.array([0.0, 0.0, 0.0, 1.0])]
     )
+
+def gaussian_vmf_sample(key, pose_mean, var, concentration):
+    return pose_mean.dot(gaussian_vmf(key, var, concentration))
+
+def gaussian_vmf_logpdf(pose, pose_mean, var, concentration):
+    translation_prob = tfp.distributions.MultivariateNormalDiag(pose_mean[:3,3], jnp.ones(3) * var).log_prob(pose[:3,3])
+    quat_mean = rotation_matrix_to_quaternion(pose_mean[:3,:3])
+    quat = rotation_matrix_to_quaternion(pose[:3,:3])
+    quat_prob = tfp.distributions.VonMisesFisher(
+        quat_mean, concentration
+    ).log_prob(quat)
+    return translation_prob + quat_prob
+
+
--- test/test_quat_rot_matrix_conversion.py
+++ test/test_quat_rot_matrix_conversion.py
@@ -1,10 +1,12 @@
 from jax3dp3.transforms_3d import *
-from jax3dp3.distributions import gaussian_vmf
+from jax3dp3.distributions import *
 import jax
 import jax.numpy as jnp
+from tensorflow_probability.substrates import jax as tfp
 
 key = jax.random.PRNGKey(3) 
-random_rot = gaussian_vmf(key, 0.1, 100.0)[:3,:3]
+random_pose = gaussian_vmf(key, 0.1, 100.0)[:3,:3]
+random_rot = random_pose[:3,:3]
 quat = rotation_matrix_to_quaternion(random_rot)
 recovered_rot = quaternion_to_rotation_matrix(quat)
 print(jnp.sum(jnp.abs(random_rot - recovered_rot)))
@@ -14,5 +16,21 @@ backward = jax.jit(quaternion_to_rotation_matrix)
 
 backward(forward(random_rot))
 
+var = 0.1
+conc = 100.0
+
+key = jax.random.PRNGKey(3)
+key2 = jax.random.PRNGKey(4)
+a = gaussian_vmf(key, var, conc)
+b = gaussian_vmf(key2, var, conc)
+
+gaussian_vmf_logpdf(a,b,var,conc)
+
+f = jax.jit(gaussian_vmf_logpdf)
+f(a,b,var,conc)
+
+for conc in jnp.linspace(10.0, 1000.0, 10):
+    print('conc:');print(conc)
+    print('f(a,b,var,conc):');print(f(a,a,var,conc))
 
 from IPython import embed; embed()
