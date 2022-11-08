187846ab8fe99d0a0b9bbe5bbd2042fd0b7a70b2
Nishad Gothoskar <nishadg@mit.edu>
2022-11-08 21:58:46 +0000

    save

---
+++ jax3dp3/likelihood_alternate.py
@@ -0,0 +1,41 @@
+
+# @functools.partial(jax.jit, static_argnames=["r", "outlier_prob"])
+def threedp3_likelihood_alternate(
+    obs_xyz: jnp.ndarray,
+    rendered_xyz: jnp.ndarray,
+    r,
+    outlier_prob
+):
+    obs_mask = obs_xyz[ :,:,2] > 0.0
+    rendered_mask = rendered_xyz[:,:,2] > 0.0
+    rendered_xyz_patches = extract_2d_patches(rendered_xyz, (7,7))
+    counts = counts_per_pixel(
+        obs_xyz,
+        rendered_xyz_patches,
+        r,
+    )
+    num_latent_points = rendered_mask.sum()
+    probs = 1 / (((1 - outlier_prob)/num_latent_points) * 4/3 * jnp.pi * r**3) * counts + outlier_prob
+    log_probs = jnp.log(probs)
+    return jnp.sum(jnp.where(obs_mask, log_probs, 0.0))
+
+@functools.partial(
+    jnp.vectorize,
+    signature='(m),(h,w,m)->()',
+    excluded=(2,),
+)
+def counts_per_pixel(
+    data_xyz: jnp.ndarray,
+    model_xyz: jnp.ndarray,
+    r: float,
+):
+    """    Args:
+        data_xyz (jnp.ndarray): (3,)
+            3d coordinate of observed point
+        model_xyz : (filter_height, filter_width, 3),
+        r : float, sphere radius
+        outlier_prob: float
+        num_latent_points: int
+    """
+    distance = jnp.linalg.norm(data_xyz - model_xyz, axis=-1).ravel() # (4,4)
+    return jnp.sum(distance <= r)
--- jax3dp3/model.py
+++
@@ -1,9 +0,0 @@
-from .likelihood import threedp3_likelihood
-from .rendering import render_planes
-
-def make_scoring_function(shape, h, w, fx_fy, cx_cy, r, outlier_prob):
-    def scorer(pose, gt_image):
-        rendered_image = render_planes(pose, shape, h, w, fx_fy, cx_cy)
-        weight = threedp3_likelihood(gt_image, rendered_image, r, outlier_prob)
-        return weight
-    return scorer
