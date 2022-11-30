2afc98dd836a3f3cfa0f16684b6369fd7c602d82
Nishad Gothoskar <nishad@mit.edu>
2022-11-30 17:37:17 +0000

    clean up script

--- experiments/fuzzy_metaballs/fuzzy_cube.py
+++ experiments/fuzzy_metaballs/fuzzy_cube.py
@@ -12,12 +12,16 @@ from jax3dp3.rendering import render_planes_rays
 import functools
 from jax3dp3.viz.img import save_depth_image
 from jax3dp3.triangle_renderer import render_triangles
-from jax3dp3.transforms_3d import transform_from_pos, apply_transform
 import jax3dp3.transforms_3d as t3d
+import jax3dp3.camera
 import trimesh
 
-
 image_size = (120,120)
+height,width = image_size
+cx = (width-1)/2
+cy = (height-1)/2
+fx = 150.0
+fy = 150.0
 
 hyperparams = fm_render.hyperparams
 NUM_MIXTURE = 100
@@ -26,51 +30,27 @@ beta_3 = jnp.float32(np.exp(hyperparams[1]))
 beta_4 = jnp.float32(np.exp(hyperparams[2]))
 beta_5 = -jnp.float32(np.exp(hyperparams[3]))
 
-def rays_from_R_T(R,T, image_size=image_size):
-    height, width = image_size
-    cx = (width-1)/2
-    cy = (height-1)/2
-    vfov_degrees = 60
-    f = (height/np.tan((np.pi/180)*float(vfov_degrees)/2))*0.5
-    K = np.array([[f, 0, cx],[0,f,cy],[0,0,1]])
-    pixel_list = (np.array(np.meshgrid(width-np.arange(width)-1,height-np.arange(height)-1,[0]))[:,:,:,0]).reshape((3,-1)).T
-    camera_rays = (pixel_list - K[:,2])/np.diag(K)
-    camera_rays[:,-1] = 1
 
-    translation = T
+distance_away = 5.0
+object_pose = t3d.transform_from_pos(jnp.array([0.0, 0.0, 0.0]))
 
-    camera_rays = camera_rays @ np.array(R ).T 
-    trans = np.tile(translation[None],(camera_rays.shape[0],1))
+camera_pose_sweep = []
 
-    camera_starts_rays = np.stack([camera_rays,trans],1)
-    return jnp.array(camera_starts_rays)
-
-camera_angle_sweep = jnp.linspace(0.0, 2*jnp.pi, 10)
-distance = 5.0
+identity_rays = jax3dp3.camera.camera_rays_from_params(height, width, fx, fy, cx, cy)
+for angle in jnp.linspace(0.0, 2*jnp.pi, 10):
+    R = t3d.transform_from_axis_angle(jnp.array([0.0, 1.0, 0.0]), angle)[:3,:3]
+    T = R @ jnp.array([0.0, 0.0, -distance_away])
+    pose = t3d.transform_from_rot_and_pos(R,T)
+    camera_pose_sweep.append(pose)
 
 mesh = trimesh.load(os.path.join(jax3dp3.utils.get_assets_dir(),"bunny.obj"))
 trimesh_shape = (10.0*mesh.vertices)[mesh.faces] * jnp.array([1.0, -1.0, 1.0])
 
-cameras_list = []
-alpha_list = []
-all_depths = []
-
-identity_rays = rays_from_R_T(jnp.eye(3), jnp.zeros(3))
-for angle in camera_angle_sweep:
-    R = transform_from_axis_angle(jnp.array([0.0, 1.0, 0.0]), angle)[:3,:3]
-    T = R @ jnp.array([0.0, 0.0, -distance])
-
-    pose = t3d.transform_from_rot_and_pos(R,T)
-    data = render_triangles(jnp.linalg.inv(pose), trimesh_shape, identity_rays[:,0,:].reshape(1,-1,3))[0]
-
-    rays = rays_from_R_T(R,T)
-    cameras_list.append(rays)
-
-    alpha_list.append(data[:,2] != 0.0)
-    all_depths.append(data[:,2])
-
-
-gt_viz_images = [get_depth_image(sil.reshape(image_size)) for sil in alpha_list]
+gt_depth_images = [
+    render_triangles(jnp.linalg.inv(cam_pose).dot(object_pose), trimesh_shape, identity_rays)
+    for cam_pose in camera_pose_sweep
+]
+gt_viz_images = [get_depth_image(depth[:,:,2], max=10.0) for depth in gt_depth_images]
 multi_panel(
     gt_viz_images,
     None,
@@ -80,6 +60,20 @@ multi_panel(
 ).save("all_views.png")
 
 
+all_camera_rays = []
+for camera_pose in camera_pose_sweep:
+    translation = camera_pose[:3,3]
+    camera_rays = identity_rays.reshape(-1,3) @ np.array(camera_pose[:3,:3]).T 
+    trans = np.tile(translation[None],(camera_rays.shape[0],1))
+    camera_starts_rays = np.stack([camera_rays,trans],1)
+    all_camera_rays.append(camera_starts_rays)
+cameras_list = all_camera_rays
+all_camera_rays = jnp.concatenate(all_camera_rays)
+
+all_silhouettes = []
+for depth in gt_depth_images:
+    all_silhouettes.append((depth[:,:,2] != 0.0).ravel())
+all_silhouettes = jnp.concatenate(all_silhouettes)
 
 def objective(params,true_alpha):
     CLIP_ALPHA = 1e-6
@@ -92,17 +86,14 @@ def objective(params,true_alpha):
     return mask_loss.mean()
 grad_render3 = jax.jit(jax.value_and_grad(objective))
 
-all_cameras = jnp.array(cameras_list).reshape((-1,2,3))
-all_sils = jnp.array(alpha_list).ravel().astype(jnp.float32)
-
-center = jnp.array([0.0, 0.0, 0.0])
+center = object_pose[:3,-1]
 shape_scale = 2.0
 rand_mean = center+np.random.multivariate_normal(mean=[0,0,0],cov=1e-1*np.identity(3)*shape_scale,size=NUM_MIXTURE)
 rand_weight_log = jnp.log(np.ones(NUM_MIXTURE)/NUM_MIXTURE)
 rand_sphere_size = 14
 rand_prec = jnp.array([np.identity(3)*rand_sphere_size/shape_scale for _ in range(NUM_MIXTURE)])
 
-init_t,stds,est_alpha = fm_render.render(rand_mean, rand_prec, rand_weight_log, all_cameras, beta_2, beta_3, beta_4, beta_5)
+init_t,stds,est_alpha = fm_render.render(rand_mean, rand_prec, rand_weight_log, all_camera_rays, beta_2, beta_3, beta_4, beta_5)
 
 from jax.example_libraries import optimizers
 
@@ -128,22 +119,16 @@ optimization_images = []
 scaling_factor = 4
 image_size_visualization = (image_size[0]*scaling_factor, image_size[1]*scaling_factor)
 
-cam_idx = 5
 
 optimization_depths = []
+opt_params_over_time = []
 for i in loop:
     p = opt_params(opt_state)
-    params = [p[0],p[1],p[2], all_cameras,beta_2,beta_3,beta_4,beta_5]
-    val,g = grad_render3(params, all_sils)
+    params = [p[0],p[1],p[2], all_camera_rays,beta_2,beta_3,beta_4,beta_5]
+    val,g = grad_render3(params, all_silhouettes)
     opt_state = opt_update(i, g[:3], opt_state)
 
-    means,prec,weights_log,camera_rays,beta_2,beta_3,beta_4,beta_5 = params
-    depth,_,alpha = fm_render.render(means,prec,weights_log,cameras_list[cam_idx],beta_2,beta_3,beta_4,beta_5)
-
-    depth = depth.at[alpha < 0.5].set(0.0)
-    optimization_depths.append(depth)
-
-   
+    opt_params_over_time.append(opt_params(opt_state))
     val = float(val)
     losses.append(val)
     loop.set_description("total_loss = %.3f" % val)
@@ -151,30 +136,30 @@ for i in loop:
 
 min_depth=1.0
 max_depth=10.0
-ground_truth_depth = all_depths[cam_idx]
-ground_truth_alpha = alpha_list[cam_idx]
-ground_truth_depth_viz = ground_truth_depth.at[ground_truth_alpha < 0.5].set(0.0)
+cam_idx = 5
+gt_depth = gt_depth_images[5][:,:,2]
+ground_truth_img = get_depth_image(gt_depth, min=min_depth, max=max_depth).resize(image_size_visualization)
 
-ground_truth_img = get_depth_image(ground_truth_depth_viz.reshape(image_size), min=min_depth, max=max_depth)
-
-
-optimization_images = [
-    multi_panel(
-        [
-            ground_truth_img.resize(image_size_visualization),
-            get_depth_image(depth.reshape(image_size),min=min_depth, max=max_depth).resize(image_size_visualization)
-        ],
-        ["Ground Truth", "Current Shape Iteration {:03d}".format(i)],
-        10,
-        50,
-        20
+rerendering_over_time = []
+for p in opt_params_over_time:
+    params = [p[0],p[1],p[2], all_camera_rays,beta_2,beta_3,beta_4,beta_5]
+    means,prec,weights_log,camera_rays,beta_2,beta_3,beta_4,beta_5 = params
+    depth,_,alpha = fm_render.render(means,prec,weights_log, cameras_list[cam_idx],beta_2,beta_3,beta_4,beta_5)
+    depth = depth.at[alpha < 0.5].set(0.0)
+    rerendering_over_time.append(
+        get_depth_image(depth.reshape(image_size),min=min_depth, max=max_depth).resize(image_size_visualization)
     )
-    for (i,depth) in enumerate(optimization_depths)
-]
 
-
-p = opt_params(opt_state)
-
+optimization_images = []
+for (i,rerender) in enumerate(rerendering_over_time):
+    optimization_images.append(multi_panel(
+            [ground_truth_img, rerender],
+            ["Ground Truth", "Current Shape Iteration {:03d}".format(i)],
+            10,
+            50,
+            20
+        )
+    )
 
 optimization_images[0].save(
     fp="out.gif",
