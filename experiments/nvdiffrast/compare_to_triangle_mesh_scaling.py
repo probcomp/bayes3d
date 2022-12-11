6b51608f5be673d8f249b77ee9a6019ef7a00a31
Nishad Gothoskar <nishad@mit.edu>
2022-12-11 05:01:57 +0000

    save

---
+++ experiments/nvdiffrast/compare_to_triangle_mesh_scaling.py
@@ -0,0 +1,26 @@
+import os
+
+from jax3dp3.viz import save_depth_image
+import jax.numpy as jnp
+import jax3dp3.camera
+import jax3dp3.utils
+import jax3dp3.triangle_renderer
+import jax3dp3.transforms_3d as t3d
+import jax
+import time
+import trimesh
+
+height, width = 200, 200
+cx = (width-1)/2
+cy = (height-1)/2
+fx = 200.0
+fy = 200.0
+rays = jax3dp3.camera.camera_rays_from_params(height, width, fx, fy, cx, cy)
+
+mesh = trimesh.load(os.path.join(jax3dp3.utils.get_assets_dir(),"cube.obj"))
+trimesh_shape = mesh.vertices[mesh.faces]
+pose = t3d.transform_from_pos(jnp.array([0.0, 0.0, 5.0]))
+num_images = 2000
+poses_all = jnp.array([pose for _ in range(num_images)])
+parallel_renderer = jax.jit(jax.vmap(lambda pose: jax3dp3.triangle_renderer.render_triangles(pose, trimesh_shape, rays)))
+parallel_renderer(poses_all)
---
+++ experiments/nvdiffrast/nvdiffrast_bunny.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+import imageio
+import os
+import numpy as np
+import torch
+import nvdiffrast.torch as dr
+import sys
+import trimesh
+import jax3dp3.utils
+import jax3dp3.viz
+import jax3dp3.transforms_3d as t3d
+import time
+import os
+from jax3dp3.viz import save_depth_image
+import jax.numpy as jnp
+import jax3dp3.camera
+import jax3dp3.triangle_renderer
+import jax3dp3.transforms_3d as t3d
+import jax
+import time
+import trimesh
+from jax3dp3.nv_rendering import render_depth, projection_matrix
+
+def tensor(*args, **kwargs):
+    return torch.tensor(*args, device='cuda', **kwargs)
+
+def zeros(size):
+    return torch.zeros(size, device='cuda')
+
+
+h, w = 120,160
+cx = (w-1)/2
+cy = (h-1)/2
+fx = 200.0
+fy = 200.0
+near=0.05
+far=10.0
+
+max_depth = 6.0
+glenv = dr.RasterizeGLContext()
+
+
+mesh = trimesh.load(os.path.join(jax3dp3.utils.get_assets_dir(),"bunny.obj"))
+vertices_orig = np.array(mesh.vertices)
+vertices = vertices_orig.copy()
+pose = t3d.transform_from_pos(jnp.array([0.0, 0.0, 4.2]))
+view_space_vertices = t3d.apply_transform(vertices, pose)
+vertices = tensor(np.array([view_space_vertices]))
+num_images = 1000
+vertices = vertices.tile((num_images,1,1))
+triangles = tensor(mesh.faces , dtype=torch.int32)
+
+# for i in range(num_images):
+#     vertices[i,:,0] -= i*0.0001
+
+point_cloud = render_depth(glenv, vertices, triangles, h,w,fx,fy,cx,cy, near, far)
+start = time.time()
+point_cloud = render_depth(glenv, vertices, triangles, h,w,fx,fy,cx,cy, near, far)
+end = time.time()
+print ("Time elapsed:", end - start)
+
+depth_img = point_cloud[-1,:,:,2]
+
+
+print('depth_img.max():');print(depth_img.max())
+print('depth_img.min():');print(depth_img.min())
+jax3dp3.viz.save_depth_image(np.array(depth_img.cpu()), "bunny.png",max=max_depth)
+
+
+
+# import jax.numpy as jnp
+
+# rays = jax3dp3.camera.camera_rays_from_params(h, w, fx, fy, cx, cy)
+# trimesh_shape = mesh.vertices[mesh.faces]
+# img = jax3dp3.triangle_renderer.render_triangles(pose, trimesh_shape, rays)
+# save_depth_image(img[:,:,2], "triangle.png", max=max_depth)
+
+from IPython import embed; embed()
