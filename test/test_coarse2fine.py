8e7130374b82877eaebaa411f5ea6dcb17b1af06
karen-sy <karenchung117@gmail.com>
2022-12-12 22:35:11 +0000

    testing and results processing updates

--- test/test_coarse2fine.py
+++ test/test_coarse2fine.py
@@ -3,8 +3,9 @@ sys.path.append('.')
 
 from functools import partial 
 import numpy as np
-import jax.numpy as jnp
 import jax
+from jax.experimental import host_callback
+import jax.numpy as jnp
 from jax3dp3.batched_scorer import batched_scorer_parallel
 from jax3dp3.coarse_to_fine import coarse_to_fine, coarse_to_fine_pose_and_weights
 from jax3dp3.enumerations import get_rotation_proposals, make_translation_grid_enumeration
@@ -59,37 +60,23 @@ enumeration_grid_tr = [make_translation_grid_enumeration(
                     ) for (_, grid_width, num_grid_points) in schedule_tr]
 enumeration_grid_r = [get_rotation_proposals(fib_nums, rot_nums) for (fib_nums, rot_nums) in schedule_rot]
 
-# fine-only "schedule": enumerate over same range as initial coarse sweep of c2f, 
-# but with the finest likelihood/grid resolution of c2f (i.e. more gridpoints)
-finest_r = schedule_tr[-1][0]  # 0.02
-coarsest_gridrange = schedule_tr[0][1]  # 1
-finest_grid_resolution = schedule_tr[-1][1] / schedule_tr[-1][-1]  # 0.1/10=0.01
-schedule_tr_fine = [(finest_r, coarsest_gridrange, int(coarsest_gridrange/finest_grid_resolution))]
-schedule_rot_fine = [schedule_rot[-1]]
+# TODO: also make "schedule" for fine-only comparison
 
-# enumeration_likelihood_r_fine = [schedule_tr_fine[-1][0]]
-# enumeration_grid_tr_fine = [make_translation_grid_enumeration(
-#                         -grid_width, -grid_width, -grid_width,
-#                         grid_width, grid_width, grid_width,
-#                         num_grid_points,num_grid_points,num_grid_points
-#                     ) for (_, grid_width, num_grid_points) in schedule_tr_fine]
-# enumeration_grid_r_fine = [get_rotation_proposals(fib_nums, rot_nums) for (fib_nums, rot_nums) in schedule_rot_fine]
+"""
+Setup testing (see test/test_generate_enumerations.py)
 
-######
-# Setup testing (see test/test_generate_enumerations.py)
+Test Shapes:
+Rectangular prism 0: (cube)
+Rectangular prism 1: (near-cube)
+Rectangular prism 2: (2 similar dims, 1 unsimilar dim)
+Rectangular prism 3: (3 unsimilar dims)
+Rectangular prism 4: (near-(square)plane)
 
-# Test Shapes:
-# Rectangular prism 0: (cube)
-# Rectangular prism 1: (near-cube)
-# Rectangular prism 2: (2 similar dims, 1 unsimilar dim)
-# Rectangular prism 3: (3 unsimilar dims)
-# Rectangular prism 4: (near-(square)plane)
-
-# Test GT Poses:
-# 4x4x4 translation enumerations
-# 8x8 rotation enumerations
-# (4096 pose proposals to test)
-######
+Test GT Poses:
+4x4x4 translation enumerations
+8x8 rotation enumerations
+(4096 pose proposals to test)
+"""
 
 # generate test shapes
 x0, y0, z0 = 0.0, 0.0, 1.50
@@ -103,7 +90,7 @@ default_pose = jnp.array([
 
 shapes_dims = jnp.array([jnp.array([0.5, 0.5, 0.5]), jnp.array([0.45, 0.5, 0.55]), jnp.array([0.40, 0.42, 0.8]), jnp.array([0.3, 0.5, 0.7]), jnp.array([0.05, 0.5, 0.5])])
 shapes = [get_rectangular_prism_shape(shape_dims) for shape_dims in shapes_dims]
-num_shapes = len(shapes)
+num_shapes = len(shapes_dims)
 print(f"Testing {num_shapes} shapes")
 
 # generate test poses and corresponding gt shapes (the ith pose has (i%5)th shape)
@@ -116,6 +103,7 @@ rotations = get_rotation_proposals(8,8)
 gt_test_poses = jnp.einsum("aij,bjk->abik", translations, rotations).reshape(-1, 4,4) 
 print(f"Testing {len(gt_test_poses)} images (GT Poses)")
 
+# generate test images
 def render_test_images(gt_test_poses):
     images = []
     for test_idx, gt_test_pose in enumerate(gt_test_poses):
@@ -128,16 +116,12 @@ gt_test_images = render_test_images(gt_test_poses)
 print(f"Rendered {len(gt_test_images)} gt test images")
 
 def get_c2f_func(shape_id):
-    # scorer_for_shape = partial(scorer, shapes[shape_id])
     scorer_for_shape = partial(scorer, get_rectangular_prism_shape(shapes_dims[shape_id]))
     coarse_to_fine_func = partial(coarse_to_fine_pose_and_weights, scorer_for_shape, enumeration_grid_tr, enumeration_grid_r, enumeration_likelihood_r)
     return coarse_to_fine_func
 
-
-from jax.experimental import host_callback
-
 def save_with_jit(x):
-  host_callback.call(lambda x: jnp.save("test_result/"+str(time.time()).split(".")[0]+'.npy', x), x)
+  host_callback.call(lambda x: jnp.save("test_result/"+str(time.time()).split(".")[0]+'.npy', x), x)   # save with timestamp postfix
 
 def get_prediction(test_idx,x):
     gt_test_pose, gt_test_image = gt_test_poses[test_idx], gt_test_images[test_idx]
@@ -150,28 +134,36 @@ def get_prediction(test_idx,x):
         return shape_i + 1, jnp.array([best_tr_idx_for_shape, best_rot_idx_for_shape, best_pose_score_for_shape])  # return pose idx, best score
     _, best_pose_and_scores = jax.lax.scan(_get_best_score_for_shape, 0, shapes)
 
-    best_shape_idx = jnp.argmax(best_pose_and_scores[:, -1])
+    best_shape_idx = jnp.argmax(best_pose_and_scores[:, -1])  # get highest-score shape
     
-    pred = best_pose_and_scores[best_shape_idx]
+    pred = jnp.array([best_shape_idx, *best_pose_and_scores[best_shape_idx]])
     
     # save_with_jit(pred)
     
     return test_idx+1, pred
 
-batchsize = 10
-result = jnp.array([0,0,0])
-for batch_i in range(2):#range(len(gt_test_poses)//batchsize):
+batchsize = 12
+result = jnp.array([jnp.inf, jnp.inf, jnp.inf, jnp.inf])
+for batch_i in range(len(gt_test_poses)//batchsize):
     start = time.time()
-    _, pred = jax.lax.scan(get_prediction, 0, gt_test_poses[10*batch_i : 10*(batch_i+1)])
+    _, pred = jax.lax.scan(get_prediction, 0, gt_test_poses[batchsize*batch_i : batchsize*(batch_i+1)])
     end = time.time()
-    print(f"{batch_i} elapsed:", end-start)
+    print(f"{batch_i}/{len(gt_test_poses)//batchsize} elapsed:", end-start)
 
     # start = time.time()
     result = jnp.vstack([result, pred])
     # end = time.time()
     # print("process time=", end-start)
 
-result
-host_callback.call(lambda x: jnp.save("predictions.npy", x), result[1:])  
+with open('predictions.npy', 'wb') as f:
+    jnp.save(f, result[1:])
+
+with open('gt_test_poses.npy', 'wb') as f:
+    jnp.save(f, gt_test_poses)
+
+
+# with open('predictions.npy', 'rb') as f:
+#     a = np.load(f)
+
 
 from IPython import embed; embed()
