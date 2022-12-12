7279e5aa7dd9d301531fea5d12f844e37628868f
Nishad Gothoskar <nishad@mit.edu>
2022-12-12 21:29:18 +0000

    Save

--- test/test_pyopengl.py
+++ test/test_pyopengl.py
@@ -43,19 +43,17 @@ def projection_matrix(h, w, fx, fy, cx, cy, near, far):
     )
     return orth @ persp @ view
 
-h, w = 200,200
-height,width = h,w
-cx = (w-1)/2
-cy = (h-1)/2
+height, width = 200, 200
+h,w = height, width
+cx = (width-1)/2
+cy = (height-1)/2
 fx = 200.0
 fy = 200.0
-near=0.05
-far=10.0
+near = 0.001
+far = 100.0
 
 P = projection_matrix(h, w, fx, fy, cx, cy, near, far)
 perspective_matrix = tuple(P.T.astype('f4').reshape(-1))
-
-
 
 glfw.init()
 # Set window hint NOT visible
@@ -65,8 +63,35 @@ window = glfw.create_window(width, height, "hidden window", None, None)
 # Make the window's context current
 glfw.make_context_current(window)
 
-glEnable(GL_DEPTH_TEST)
-glClear(GL_DEPTH_BUFFER_BIT)
+glDisable(GL_CULL_FACE)
+glEnable(GL_DEPTH_TEST)  # https://www.khronos.org/opengles/sdk/docs/man/xhtml/glEnable.xml
+glClearColor(0., 0., 0., 1.0)
+glClearDepth(1.0)
+glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+fbo = glGenFramebuffers(1)
+glBindFramebuffer(GL_FRAMEBUFFER, fbo)
+
+color_tex = glGenTextures(1)
+glBindTexture(GL_TEXTURE_2D, color_tex)
+glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, width, height, 0,
+                GL_RGBA, GL_UNSIGNED_BYTE, None)
+glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, color_tex, 0)
+
+depth_tex = glGenTextures(1)
+glBindTexture(GL_TEXTURE_2D, depth_tex)
+glTexImage2D.wrappedOperation(
+    GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, width, height, 0, 
+    GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, None
+);
+glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depth_tex, 0)
+
+
+if not glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE:
+    print('framebuffer binding failed')
+    exit()
+glBindFramebuffer(GL_FRAMEBUFFER, 0)
+
 
 #Now create the shaders
 VERTEX_SHADER = shaders.compileShader("""
@@ -96,30 +121,37 @@ shader = shaders.compileProgram(VERTEX_SHADER, FRAGMENT_SHADER)
 mesh = trimesh.load(os.path.join(jax3dp3.utils.get_assets_dir(),"bunny.obj"))
 vertices_orig = np.array(mesh.vertices)
 vertices = vertices_orig.copy()
-pose = t3d.transform_from_pos(np.array([0.0, 0.0, 4.2]))
+pose = t3d.transform_from_pos(np.array([0.0, 0.0, 2.0]))
 vertices = t3d.apply_transform(vertices, pose)
 indices = np.array(mesh.faces)
 
 
-vertices = np.array([
-    [0.0,0.0,3.0],[3.0,0.0,3.0],[0.0,2.0,3.0], [-3.0,-3.0,8.0]
-    ], dtype='f')
-indices = np.array([[0,1,2], [1,2,3]], dtype=np.int32)
+# vertices = np.array([
+#     [0.0,0.0,3.0],[3.0,0.0,3.0],[0.0,2.0,3.0], [-3.0,-3.0,8.0]
+#     ], dtype='f')
+# indices = np.array([[0,1,2], [1,2,3]], dtype=np.int32)
 
 vertices = np.array(vertices, dtype='f')
 indices = np.array(indices, dtype=np.int32)
 
 
+vao = glGenVertexArrays(1)
+glBindVertexArray(vao)
+
 vertexPositions = glGenBuffers(1)
 glBindBuffer(GL_ARRAY_BUFFER, vertexPositions)
 glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW)
+glVertexAttribPointer(0, 3, GL_FLOAT, False, 0, None)
+glEnableVertexAttribArray(0)
 
 indexPositions = glGenBuffers(1)
 glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexPositions)
 glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW)
 
-glEnableVertexAttribArray(0)
-glVertexAttribPointer(0, 3, GL_FLOAT, False, 0, None)
+glBindBuffer(GL_ARRAY_BUFFER, 0)
+glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)
+glBindVertexArray(0)
+
 
 
 glEnable(GL_DEPTH_TEST)
@@ -133,6 +165,8 @@ glUniformMatrix4fv(glGetUniformLocation(shader,"mvp"), 1, GL_FALSE, perspective_
 #glDrawArrays(GL_TRIANGLES, 0, 3) #This line still works
 
 
+glBindFramebuffer(GL_FRAMEBUFFER, fbo)
+glBindVertexArray(vao)
 glBindBuffer(GL_ARRAY_BUFFER, vertexPositions)
 glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexPositions)
 glDrawElements(GL_TRIANGLES, indices.shape[0]*3, GL_UNSIGNED_INT, None) #This line does work too!
@@ -141,6 +175,6 @@ im = glReadPixels(0, 0, width, height, GL_RGBA, GL_FLOAT)
 im = im.reshape(height,width,4)
 
 import jax3dp3.viz
-jax3dp3.viz.save_depth_image(im[:,:,2],"test.png", min=-5.0,max=5.0)
+jax3dp3.viz.save_depth_image(im[:,:,2],"bunny2.png", max=6.0)
 
 from IPython import embed; embed()
