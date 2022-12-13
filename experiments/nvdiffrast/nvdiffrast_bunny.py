0e5a0281ecfded5026e109846f3a365d6c9ce577
Nishad Gothoskar <nishad@mit.edu>
2022-12-13 22:16:06 +0000

    cuda in progress

--- experiments/nvdiffrast/nvdiffrast_bunny.py
+++ experiments/nvdiffrast/nvdiffrast_bunny.py
@@ -44,13 +44,13 @@ near=0.05
 far=10.0
 
 max_depth = 10.0
-glenv = dr.RasterizeCudaContext()
+glenv = dr.RasterizeGLContext(output_db=False)
 
 
 mesh = trimesh.load(os.path.join(jax3dp3.utils.get_assets_dir(),"bunny.obj"))
 vertices_orig = np.array(mesh.vertices)
 vertices = vertices_orig.copy()
-pose = t3d.transform_from_pos(jnp.array([0.0, 0.0, 4.2]))
+pose = t3d.transform_from_pos(jnp.array([0.0, 0.0, 3.2]))
 view_space_vertices = t3d.apply_transform(vertices, pose)
 vertices = tensor(np.array([view_space_vertices]))
 num_images = 1
@@ -59,30 +59,20 @@ triangles = tensor(mesh.faces , dtype=torch.int32)
 
 # for i in range(num_images):
 #     vertices[i,:,0] -= i*0.0001
+point_cloud = render_depth(glenv, vertices, triangles, h,w,fx,fy,cx,cy, near, far)
 
-point_cloud = render_depth(glenv, vertices, triangles, h,w,fx,fy,cx,cy, near, far)
 start = time.time()
-point_cloud = render_depth(glenv, vertices, triangles, h,w,fx,fy,cx,cy, near, far)
-print(point_cloud[0,0,0])
+rast = render_depth(glenv, vertices, triangles, h,w,fx,fy,cx,cy, near, far)
 end = time.time()
 print ("Time elapsed:", end - start)
+print(rast[0][0][0])
 
-depth_img = point_cloud[-1,:,:,2]
-
-
-print('depth_img.max():');print(depth_img.max())
-print('depth_img.min():');print(depth_img.min())
-jax3dp3.viz.save_depth_image(np.array(depth_img.cpu()), "bunny.png",max=max_depth)
-
+depth = rast[:,:,:,2]
+neg_mask = depth == 0
+depth = 2 * near * far / (far + near - depth * (far - near))
+depth[neg_mask] = 0
 
 
-import jax.numpy as jnp
-
-rays = jax3dp3.camera.camera_rays_from_params(h, w, fx, fy, cx, cy)
-trimesh_shape = mesh.vertices[mesh.faces]
-img = jax3dp3.triangle_renderer.render_triangles(pose, trimesh_shape, rays)
-print('img[:,:,2].max():');print(img[:,:,2].max())
-print('img[:,:,2].min():');print(img[:,:,2].min())
-save_depth_image(img[:,:,2], "triangle.png", max=max_depth)
+jax3dp3.viz.save_depth_image(depth[0,:,:,].cpu().numpy(), "bunny.png",max=10.0)
 
 from IPython import embed; embed()
--- jax3dp3/nv_rendering.py
+++ jax3dp3/nv_rendering.py
@@ -40,14 +40,4 @@ def render_depth(glenv, vertices, triangles, h,w,fx,fy,cx,cy, near, far):
     view_space_vertices_h = torch.concatenate([vertices, torch.ones((*vertices.shape[:-1],1) , device='cuda')],axis=-1)
     clip_space_vertices = torch.einsum("ij,abj->abi", proj, view_space_vertices_h).contiguous()
     rast, _ = dr.rasterize(glenv, clip_space_vertices, triangles, resolution=[h,w], grad_db=False)
-    depth = rast[:,:,:,2]
-    neg_mask = depth == 0
-    depth = 2 * near * far / (far + near - depth * (far - near))
-    depth[neg_mask] = 0
-
-    xs= torch.linspace(0, w - 1, w, device='cuda')
-    ys= torch.linspace(0, h - 1, h, device='cuda')
-    vu = (torch.stack(torch.meshgrid([ys, xs], indexing="ij"),axis=-1) - torch.tensor([cy,cx],device='cuda')) / torch.tensor([fy,fx],device='cuda')
-    vu = torch.concatenate([vu, torch.ones((*vu.shape[:-1],1), device='cuda')],axis=-1)
-    point_cloud = (vu[None,:,:,:] * depth[:,:,:,None])
-    return point_cloud
+    return rast
