82e08fbfd423201536143edceb4d9cac2ffe0062
Nishad Gothoskar <nishad@mit.edu>
2023-01-06 16:46:57 +0000

    Make khaleds script work again after refactor

--- experiments/cognitive-battery/cog_intro.py
+++ experiments/cognitive-battery/cog_intro.py
@@ -1,28 +1,19 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from jax3dp3.viz import save_depth_image, get_depth_image, multi_panel
-from jax3dp3.utils import depth_to_coords_in_camera
-from jax3dp3.transforms_3d import transform_from_pos
-from jax3dp3.rendering.shape import (
-    get_rectangular_prism_shape,
-)
-from jax3dp3.likelihood import threedp3_likelihood
+import jax3dp3.transforms_3d as t3d
 import jax.numpy as jnp
 import jax
+import jax3dp3
 from scipy.spatial.transform import Rotation as R
-from jax3dp3.rendering.jax_rendering import render_planes_multiobject
-from jax3dp3.enumerations import make_translation_grid_enumeration
-from jax3dp3.enumerations_procedure import enumerative_inference_single_frame
-from jax3dp3.batched_scorer import batched_scorer_parallel
-from jax3dp3.viz.gif import make_gif_from_pil_images
+import jax3dp3.jax_rendering
 from PIL import Image
 
 from tqdm import tqdm
 # Initialize metadata
 
 num_frames = 103
-data_path = "data/swap_data/videos"
+data_path = "data/videos"
 
 width =  300
 height =  300
@@ -65,7 +56,7 @@ for frame_idx in range(num_frames):
     # frame_idx = 20
     k = 5 if 5 <= frame_idx < 19 else 4 # 4 objects in frames [5:19]
 
-    coord_image,_ = depth_to_coords_in_camera(depth_images[frame_idx], K)
+    coord_image,_ = t3d.depth_to_coords_in_camera(depth_images[frame_idx], K)
     segmentation_image = seg_maps[frame_idx]
     mask = np.invert(
         (coord_image[:,:,0] < 1.0) *
@@ -92,8 +83,8 @@ seg_img = seg_images[start_t][:,:,2]
 
 imgs = []
 for obj_id in jnp.unique(seg_img):
-    imgs.append(get_depth_image(seg_img == obj_id))
-multi_panel(imgs, [], 10, 10, 13).save("masks.png")
+    imgs.append(jax3dp3.viz.get_depth_image(seg_img == obj_id))
+jax3dp3.viz.multi_panel(imgs, [], 10, 10, 13).save("masks.png")
 
 for obj_id in jnp.unique(seg_img):
     if obj_id == 0:
@@ -106,10 +97,10 @@ for obj_id in jnp.unique(seg_img):
     dims = (maxs - mins)
     center_of_box = (maxs+mins)/2
     
-    init_pose = transform_from_pos(center_of_box)
+    init_pose = t3d.transform_from_pos(center_of_box)
     init_poses.append(init_pose)
 
-    shape, dim = get_rectangular_prism_shape(dims)
+    shape, dim = jax3dp3.jax_rendering.get_rectangular_prism_shape(dims)
     # print('dims:');print(dims)
     # print('center_of_box:');print(center_of_box)
     # print("\n")
@@ -121,7 +112,7 @@ shape_planes = jnp.stack(shape_planes)
 shape_dims = jnp.stack(shape_dims)
 init_poses = jnp.stack(init_poses)
 
-reconstruction_image = render_planes_multiobject(
+reconstruction_image = jax3dp3.jax_rendering.render_planes_multiobject(
     init_poses,
     shape_planes,
     shape_dims,
@@ -130,12 +121,12 @@ reconstruction_image = render_planes_multiobject(
 
 def render_planes_multiobject_lambda(poses):
     return (
-        render_planes_multiobject(poses, shape_planes, shape_dims, height, width, fx,fy, cx,cy)
+        jax3dp3.jax_rendering.render_planes_multiobject(poses, shape_planes, shape_dims, height, width, fx,fy, cx,cy)
     )
 render_planes_multiobject_jit = jax.jit(render_planes_multiobject_lambda)
 
 reconstruction_image = render_planes_multiobject_jit(init_poses)
-save_depth_image(reconstruction_image[:,:,2], "reconstruction.png", max=5.0)
+jax3dp3.viz.save_depth_image(reconstruction_image[:,:,2], "reconstruction.png", max=5.0)
 
 
 # reconstruction_image = render_planes_multiobject_jit(init_poses)
@@ -146,8 +137,8 @@ r = 0.005
 outlier_prob = 0.01
 def likelihood(x, params):
     obs= params[0]
-    rendered_image = render_planes_multiobject(x, shape_planes, shape_dims, height, width, fx,fy, cx,cy)
-    weight = threedp3_likelihood(obs, rendered_image, r, outlier_prob)
+    rendered_image = jax3dp3.jax_rendering.render_planes_multiobject(x, shape_planes, shape_dims, height, width, fx,fy, cx,cy)
+    weight = jax3dp3.threedp3_likelihood(obs, rendered_image, r, outlier_prob)
     return weight
 
 likelihood_parallel = jax.vmap(likelihood, in_axes = (0, None))
@@ -155,7 +146,7 @@ likelihood_parallel_jit = jax.jit(likelihood_parallel)
 
 
 
-enumerations = make_translation_grid_enumeration(-0.1, -0.1, -0.1, 0.1, 0.1, 0.1, 9, 9, 9)
+enumerations = jax3dp3.make_translation_grid_enumeration(-0.1, -0.1, -0.1, 0.1, 0.1, 0.1, 9, 9, 9)
 
 cm = plt.get_cmap("turbo")
 max_depth = 30.0
@@ -166,9 +157,9 @@ top_border = 100
 pose_estimates = init_poses.copy()
 
 inferred_poses = []
-batched_scorer_parallel_jit = jax.jit(lambda poses, image: batched_scorer_parallel(likelihood_parallel, 9, poses, (image,)))
+batched_scorer_parallel_jit = jax.jit(lambda poses, image: jax3dp3.jax_rendering.batched_scorer_parallel_params(likelihood_parallel, 9, poses, (image,)))
 
-num_steps = 90
+num_steps = 2
 for t in tqdm(range(start_t, start_t+num_steps)):
     gt_image = jnp.array(coord_images[t])
     for i in range(pose_estimates.shape[0]):
@@ -189,14 +180,14 @@ for t in range(start_t, start_t+num_steps):
     rgb_viz = Image.fromarray(
         rgb_images[t].astype(np.int8), mode="RGB"
     )
-    gt_depth_1 = get_depth_image(coord_images[t][:,:,2], max=5.0)
+    gt_depth_1 = jax3dp3.viz.get_depth_image(coord_images[t][:,:,2], max=5.0)
     depth = render_planes_multiobject_jit(inferred_poses[t-start_t])
-    depth = get_depth_image(depth[:,:,2], max=5.0)
+    depth = jax3dp3.viz.get_depth_image(depth[:,:,2], max=5.0)
     all_images.append(
-        multi_panel([rgb_viz, gt_depth_1, depth], ["RGB Image", "Actual Depth", "Reconstructed Depth"], middle_width=10, top_border=100, fontsize=20)
+        jax3dp3.viz.multi_panel([rgb_viz, gt_depth_1, depth], ["RGB Image", "Actual Depth", "Reconstructed Depth"], middle_width=10, top_border=100, fontsize=20)
     )
 
-make_gif_from_pil_images(all_images, "out.gif")
+jax3dp3.viz.make_gif_from_pil_images(all_images, "out.gif")
 
 
 from IPython import embed; embed()
