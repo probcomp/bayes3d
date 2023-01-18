a2f84393228aab07a0184b080419b766b737778e
karen-sy <karenchung117@gmail.com>
2023-01-18 00:53:43 +0000

    viz dashboard

--- experiments/classification/panda_classification_c2f.py
+++ experiments/classification/panda_classification_c2f.py
@@ -15,16 +15,16 @@ for scene_num in [1,3,4,6]:
     file = open(os.path.join(panda_dataset_path, f"scene_{scene_num}.pkl"),'rb')
     all_data = pickle.load(file)
     file.close()
-    t = -1
+    t = frame_num = -1
     data = all_data[t]
 
-    rgb = data["rgb"]
-    rgb_viz = jax3dp3.viz.get_rgb_image(rgb, 255.0)
-    rgb_viz.save("rgb.png")
-    depth = data["depth"] / 1000.0
-    jax3dp3.viz.get_depth_image(depth, max=3000.0).save("depth.png")
+    rgb_data = data["rgb"]
+    rgb_viz = jax3dp3.viz.get_rgb_image(rgb_data, 255.0)
+    # rgb_viz.save("rgb.png")
+    depth_data = data["depth"] / 1000.0
+    # jax3dp3.viz.get_depth_image(depth_data, max=3000.0).save("depth.png")
     K = data["intrinsics"][0]
-    orig_h,orig_w = depth.shape
+    orig_h,orig_w = depth_data.shape
     orig_fx, orig_fy, orig_cx, orig_cy = K[0,0],K[1,1],K[0,2],K[1,2]
     near = 0.001
     far = 5.0
@@ -32,8 +32,8 @@ for scene_num in [1,3,4,6]:
     scaling_factor = 0.3
     max_depth = far
     h,w,fx,fy,cx,cy = jax3dp3.camera.scale_camera_parameters(orig_h,orig_w,orig_fx,orig_fy,orig_cx,orig_cy, scaling_factor)
-    depth = jax3dp3.utils.resize(depth, h, w)
-    rgb_viz_resized = jax3dp3.viz.resize_image(rgb_viz, h,w)
+    depth_data = jax3dp3.utils.resize(depth_data, h, w)
+    # rgb_viz_resized = jax3dp3.viz.resize_image(rgb_viz, h,w)
 
 
     jax3dp3.setup_renderer(h, w, fx, fy, cx, cy, near, far)
@@ -54,17 +54,17 @@ for scene_num in [1,3,4,6]:
 
 
     ### segment the image
-    gt_image_full = t3d.depth_to_point_cloud_image(depth, fx,fy,cx,cy)
-    jax3dp3.viz.save_depth_image(gt_image_full[:,:,2], "gt_image_full.png", max=far)
+    gt_image_full = t3d.depth_to_point_cloud_image(depth_data, fx,fy,cx,cy)
+    # jax3dp3.viz.save_depth_image(gt_image_full[:,:,2], "gt_image_full.png", max=far)
     gt_point_cloud_full = t3d.point_cloud_image_to_points(gt_image_full)
     table_pose, table_dims = jax3dp3.utils.find_table_pose_and_dims(gt_point_cloud_full[gt_point_cloud_full[:,2] < far, :], 
     ransac_threshold=0.001, inlier_threshold=0.002, segmentation_threshold=0.004)
 
     gt_image_above_table = gt_image_full * (t3d.apply_transform(gt_image_full, jnp.linalg.inv(table_pose))[:,:,2] > 0.02)[:,:,None]
-    jax3dp3.viz.save_depth_image(gt_image_above_table[:,:,2], "gt_image_above_table.png", max=far)
+    # jax3dp3.viz.save_depth_image(gt_image_above_table[:,:,2], "gt_image_above_table.png", max=far)
 
     segmentation_img = jax3dp3.utils.segment_point_cloud_image(gt_image_above_table, threshold=0.01, min_points_in_cluster=30)
-    jax3dp3.viz.save_depth_image(segmentation_img + 1, "seg.png", max=segmentation_img.max() + 1)
+    # jax3dp3.viz.save_depth_image(segmentation_img + 1, "seg.png", max=segmentation_img.max() + 1)
     unique, counts =  np.unique(segmentation_img, return_counts=True)
 
     table_face_param = 2
@@ -83,30 +83,30 @@ for scene_num in [1,3,4,6]:
         jnp.arange(6)
     )
 
-    grid_width = 0.07
-    contact_param_sweep_2, face_param_sweep_2 = jax3dp3.scene_graph.enumerate_contact_and_face_parameters(
-        -grid_width, -grid_width, 0.0, +grid_width, +grid_width, jnp.pi*2, 
-        5, 5, 15,
-        jnp.arange(6)
-    )
+    # grid_width = 0.07
+    # contact_param_sweep_2, face_param_sweep_2 = jax3dp3.scene_graph.enumerate_contact_and_face_parameters(
+    #     -grid_width, -grid_width, 0.0, +grid_width, +grid_width, jnp.pi*2, 
+    #     5, 5, 15,
+    #     jnp.arange(6)
+    # )
 
-    grid_width = 0.04
-    contact_param_sweep_3, face_param_sweep_3 = jax3dp3.scene_graph.enumerate_contact_and_face_parameters(
-        -grid_width, -grid_width, 0.0, +grid_width, +grid_width, jnp.pi*2, 
-        5, 5, 15,
-        jnp.arange(6)
-    )
+    # grid_width = 0.04
+    # contact_param_sweep_3, face_param_sweep_3 = jax3dp3.scene_graph.enumerate_contact_and_face_parameters(
+    #     -grid_width, -grid_width, 0.0, +grid_width, +grid_width, jnp.pi*2, 
+    #     5, 5, 15,
+    #     jnp.arange(6)
+    # )
 
-    grid_width = 0.02
-    contact_param_sweep_4, face_param_sweep_4 = jax3dp3.scene_graph.enumerate_contact_and_face_parameters(
-        -grid_width, -grid_width, 0.0, +grid_width, +grid_width, jnp.pi*2, 
-        5, 5, 15,
-        jnp.arange(6)
-    )
+    # grid_width = 0.02
+    # contact_param_sweep_4, face_param_sweep_4 = jax3dp3.scene_graph.enumerate_contact_and_face_parameters(
+    #     -grid_width, -grid_width, 0.0, +grid_width, +grid_width, jnp.pi*2, 
+    #     5, 5, 15,
+    #     jnp.arange(6)
+    # )
 
-    contact_param_sched = [contact_param_sweep, contact_param_sweep_2, contact_param_sweep_3, contact_param_sweep_4]
-    face_param_sched = [face_param_sweep, face_param_sweep_2, face_param_sweep_3, face_param_sweep_4]
-    likelihood_r_sched = [0.2, 0.1, 0.05, 0.02]
+    contact_param_sched = [contact_param_sweep]#, contact_param_sweep_2, contact_param_sweep_3, contact_param_sweep_4]
+    face_param_sched = [face_param_sweep]#, face_param_sweep_2, face_param_sweep_3, face_param_sweep_4]
+    likelihood_r_sched = [0.2]#, 0.1, 0.05, 0.02]
 
     outlier_prob, outlier_volume = 0.1, 10**3
 
@@ -142,6 +142,9 @@ for scene_num in [1,3,4,6]:
         end= time.time()
         print ("Time elapsed:", end - start)
 
+
+        gt_panels_viz = jax3dp3.c2f.multi_panel_gt_images(rgb_data, gt_image_full, gt_image_above_table, segmentation_img, h,w, max_depth, title=f"GT Images: Scene {scene_num} Frame {frame_num}", scene_id=scene_num)
+        gt_panels_viz.save(f"imgs/gt_panels_scene_{scene_num}_frame_{frame_num}.png")
 
         panel_viz = jax3dp3.c2f.multi_panel_c2f(
             results, likelihood_r_sched[-1], gt_img_complement, gt_image_masked, 
--- jax3dp3/c2f.py
+++ jax3dp3/c2f.py
@@ -67,6 +67,58 @@ def c2f_contact_parameters(
     return items
 
 
+def multi_panel_gt_images(rgb_data, gt_image_full_data, gt_image_above_table_data, segmentation_image_data, h, w, max_depth, title="GT Images", img_dir="imgs",scene_id=0):
+    imgs = []  # add images of dim h-by-w
+    labels = []
+    scores = []
+    imgs = []
+    
+    # add original RGB image
+    rgb_viz = jax3dp3.viz.get_rgb_image(rgb_data, 255.0)
+    rgb_viz = jax3dp3.viz.resize_image(rgb_viz, h,w)
+    imgs.append(rgb_viz)
+    labels.append("GT RGB Image (full)")
+    rgb_viz.save(f"{img_dir}/rgb_{scene_id}.png")
+
+    # add original GT depth image
+    # gt_image_data = jax3dp3.transforms_3d.depth_to_point_cloud_image(depth_data, fx,fy,cx,cy)
+    gt_image_full = jax3dp3.viz.get_depth_image(gt_image_full_data[:,:,2], max=max_depth)
+    imgs.append(gt_image_full)
+    labels.append("GT Depth Image (full)")
+    gt_image_full.save(f"{img_dir}/gt_image_full_{scene_id}.png")
+
+    # add above-table GT image
+    # gt_point_cloud_full = jax3dp3.transforms_3d.point_cloud_image_to_points(gt_image_data)
+    # table_pose, _ = jax3dp3.utils.find_table_pose_and_dims(gt_point_cloud_full[gt_point_cloud_full[:,2] < max_depth, :], 
+    # ransac_threshold=0.001, inlier_threshold=0.002, segmentation_threshold=0.004)
+    # gt_image_above_table_data = gt_image_full * (jax3dp3.transforms_3d.apply_transform(gt_image_full, jnp.linalg.inv(table_pose))[:,:,2] > 0.02)[:,:,None]
+    gt_image_above_table_viz = jax3dp3.viz.get_depth_image(gt_image_above_table_data[:,:,2], max=max_depth)
+    imgs.append(gt_image_above_table_viz)
+    labels.append("GT Depth Image (above table)")
+    gt_image_above_table_viz.save(f"{img_dir}/gt_image_above_table_{scene_id}.png")
+
+    # add segmented depth image
+    segmentation_image_viz = jax3dp3.viz.get_depth_image(segmentation_image_data + 1, max=segmentation_image_data.max() + 1)
+    imgs.append(segmentation_image_viz)
+    labels.append("Segmentation Results")
+    segmentation_image_viz.save(f"{img_dir}/segmentation_image_{scene_id}.png")
+
+    # add segmentation-overlaid GT image
+    segmentation_on_gt_viz = jax3dp3.viz.overlay_image(rgb_viz, segmentation_image_viz)
+    imgs.append(segmentation_on_gt_viz)
+    labels.append("Segmentation overlap on GT")
+
+    dst = jax3dp3.viz.multi_panel(
+        [*imgs],
+        labels=[*labels],
+        label_fontsize =15,
+        title=title
+    )
+
+    return dst
+
+
+
 def multi_panel_c2f(results:list, r, gt_img_complement, gt_image_masked, rgb_viz, h, w, max_depth, outlier_prob, outlier_volume, model_names,title=None):
     overlays = []
     labels = []
