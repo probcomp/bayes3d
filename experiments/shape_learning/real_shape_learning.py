bae78e7988e077482cfc62a0120cd3ea0b3f752c
Nishad Gothoskar <nishad@mit.edu>
2023-01-24 20:37:33 +0000

    real shape learning with calibration

--- experiments/shape_learning/real_shape_learning.py
+++ experiments/shape_learning/real_shape_learning.py
@@ -8,7 +8,7 @@ import pickle
 import jax3dp3.transforms_3d as t3d
 import jax3dp3
 
-jax3dp3.meshcat.setup_visualizer()
+jax3dp3.setup_visualizer()
 
 
 # filename = "panda_dataset/scene_4.pkl"
@@ -23,13 +23,36 @@ K = jnp.array([[606.92871094, 0.    , 415.18270874],
     [ 0.    , 0.    , 1.    ]])
 orig_fx, orig_fy, orig_cx, orig_cy = K[0,0],K[1,1],K[0,2],K[1,2]
 
+
+
+calibration_data = {
+  "qw": 0.7074450620054885,
+  "qx": -0.06123049355593103,
+  "qy": -0.05318248430843586,
+  "qz": 0.7020996612061071,
+  "x": 0.04268000721548824,
+  "y": -0.01696075177674074,
+  "z": 0.06000526018408979,
+}
+
+translation = jnp.array([
+    calibration_data["x"], calibration_data["y"], calibration_data["z"]
+])
+
+R = t3d.xyzw_to_rotation_matrix(jnp.array([
+    calibration_data["qx"], calibration_data["qy"], calibration_data["qz"],calibration_data["qw"],
+]))
+gripper_pose_to_cam_pose = t3d.transform_from_rot_and_pos(R, translation)
+
+
+
 pcs = []
 poses = []
 
 for t in range(4):
     rgb = all_data[t]["rgb"]
     jax3dp3.viz.save_rgb_image(rgb, 255.0, f"imgs/{t}.png")
-    depth_original = all_data[t]["depth"]/ 1000.0
+    depth_original = all_data[t]["depth"] / 1000.0
 
 
     orig_h,orig_w = depth_original.shape
@@ -37,33 +60,47 @@ for t in range(4):
 
     gt_image_full = t3d.depth_to_point_cloud_image(depth_original, fx, fy, cx, cy)
     point_cloud = t3d.point_cloud_image_to_points(gt_image_full)
-    pcs.append(point_cloud)
 
     translation = jnp.array(all_data[t]["extrinsics"][0])
     R = t3d.xyzw_to_rotation_matrix(all_data[t]["extrinsics"][1])
     cam_pose = (
-        t3d.transform_from_rot_and_pos(R, translation) @ 
-        t3d.transform_from_axis_angle(jnp.array([0.0, 0.0, 1.0]), jnp.pi/2)
+        t3d.transform_from_rot_and_pos(R, translation) 
+        @ 
+        # t3d.inverse_pose(gripper_pose_to_cam_pose)
+        gripper_pose_to_cam_pose
     )
 
 
     pcs.append(point_cloud)
     poses.append(cam_pose)
 
-jax3dp3.meshcat.clear()
-jax3dp3.meshcat.show_cloud("a", t3d.apply_transform(pcs[0], poses[0]))
-jax3dp3.meshcat.show_pose("a2", poses[0])
-jax3dp3.meshcat.show_pose("a3", poses[1])
-jax3dp3.meshcat.show_cloud("a4", t3d.apply_transform(pcs[1], poses[1]))
+
+all_clouds = []
+for (pc, pose) in zip(pcs, poses):
+    all_clouds.append(
+        t3d.apply_transform(pc, pose)
+    )
+
+jax3dp3.clear()
+
+jax3dp3.show_cloud("a", all_clouds[0])
+jax3dp3.show_pose("a2", poses[0])
+jax3dp3.show_pose("a3", poses[1])
+jax3dp3.show_pose("a4", poses[2])
+jax3dp3.show_pose("a5", poses[3])
+
+jax3dp3.show_cloud("a4", all_clouds[1])
+
+jax3dp3.show_cloud("a4", t3d.apply_transform(pcs[1], poses[1]))
 
 
-jax3dp3.meshcat.show_pose("a5", poses[2])
-jax3dp3.meshcat.show_pose("a6", poses[3])
+jax3dp3.show_pose("a5", poses[2])
+jax3dp3.show_pose("a6", poses[3])
 
 from IPython import embed; embed()
 
-jax3dp3.meshcat.show_cloud("a", pcs[0])
-jax3dp3.meshcat.show_cloud("a", pcs[0])
+jax3dp3.show_cloud("a", pcs[0])
+jax3dp3.show_cloud("a", pcs[0])
 
 
 
