800993c3cc4424beb06e35cbd9d70bdf2473f07b
karen-sy <karenchung117@gmail.com>
2023-01-30 17:43:03 +0000

    c2f with angle reduction

--- jax3dp3/c2f.py
+++ jax3dp3/c2f.py
@@ -18,6 +18,34 @@ def make_schedules(grid_widths, grid_params):
         face_param_sched.append(f)
     return contact_param_sched, face_param_sched
 
+
+def make_schedules_reduce_angle(grid_widths, grid_params):
+    ## version of make_schedules with angle range reduction based on previous iter
+    contact_param_sched = []
+    face_param_sched = []
+
+    min_angle = 0.0
+    max_angle = jnp.pi*2
+    for (grid_width, grid_param) in zip(grid_widths, grid_params):
+        c, f = jax3dp3.scene_graph.enumerate_contact_and_face_parameters(
+            -grid_width, -grid_width, min_angle, +grid_width, +grid_width, max_angle, 
+            *grid_param,
+            jnp.arange(6)
+        )
+
+        angle_range = max_angle - min_angle 
+        num_angle = grid_param[-1]
+        angle_delta = angle_range / num_angle 
+
+        # next range of angles to grid over 
+        min_angle = -angle_delta 
+        max_angle = angle_delta
+
+
+        contact_param_sched.append(c)
+        face_param_sched.append(f)
+    return contact_param_sched, face_param_sched
+
 def c2f_contact_parameters(
     init_contact_parameters,
     contact_param_sched, 
@@ -104,6 +132,99 @@ def c2f_contact_parameters(
     return final_results
 
 
+def c2f_contact_parameters_no_occlusion(
+    init_contact_parameters,
+    contact_param_sched, 
+    face_param_sched,
+    likelihood_r_sched,
+    r_overlap_check,
+    r_final,
+    contact_plane_pose,
+    gt_image_masked,
+    # gt_img_complement,    
+    model_box_dims,
+    outlier_prob,
+    outlier_volume,
+    top_k=None
+):
+    """
+    do coarse-to-fine, keeping the top top_k hypotheses at each round
+    """
+    num_steps = len(contact_param_sched)
+    num_objects = model_box_dims.shape[0] 
+    if top_k is None:
+        top_k = num_objects
+    top_k_heap = collections.deque([(float('-inf'), obj_idx, init_contact_parameters, None, None) for obj_idx in range(num_objects)])  # start with all objects inference
+    
+    for sched_i in range(num_steps):
+    
+        r = likelihood_r_sched[sched_i]
+        contact_param_sweep_delta, face_param_sweep = contact_param_sched[sched_i], face_param_sched[sched_i]
+                
+        for _ in range(len(top_k_heap)):
+            _, obj_idx, c, _, _ = top_k_heap.popleft()
+
+            contact_param_sweep = contact_param_sweep_delta + c  # shift center 
+
+            # get pose proposals in cam frame
+            pose_proposals = jax3dp3.scene_graph.pose_from_contact_and_face_params_parallel_jit(
+                contact_param_sweep,
+                face_param_sweep,
+                model_box_dims[obj_idx],
+                contact_plane_pose
+            )  
+
+            # get best pose proposal
+            images_unmasked = jax3dp3.render_parallel(pose_proposals, obj_idx)
+
+            # images = jax3dp3.renderer.get_complement_masked_images(images_unmasked, gt_img_complement)
+            images = images_unmasked  # no masking / occlusion
+            weights = jax3dp3.threedp3_likelihood_parallel_jit(gt_image_masked, images, r, outlier_prob, outlier_volume)
+            best_pose_idx = weights.argmax()
+
+            top_k_heap.append((weights[best_pose_idx], obj_idx,
+                               contact_param_sweep[best_pose_idx],
+                               face_param_sweep[best_pose_idx],
+                               pose_proposals[best_pose_idx]))  
+
+        if sched_i == 0: 
+            top_k_heap = collections.deque(heapq.nlargest(top_k, top_k_heap))  # after the first iteration prune search down to the top_k top hypothesis objects 
+        
+        # print(f"top {top_k} after {sched_i} iters:\n: {[(s, model_names[i]) for (s,i,p) in top_k_heap]}")
+
+    items = [item for item in heapq.nlargest(top_k, top_k_heap)]
+    scores = np.array([item[0] for item in items])
+    items = [items[i] for i in np.argsort(-scores)]
+
+    final_results = []
+    for i in range(len(items)):
+        score_orig, obj_idx, _, _, pose = items[i]
+        image_unmasked = jax3dp3.render_single_object(pose, obj_idx)
+        image =  image_unmasked
+        
+        overlap = jax3dp3.likelihood.threedp3_likelihood_get_counts(gt_image_masked, image, r_overlap_check)
+        score = jax3dp3.threedp3_likelihood_parallel_jit(gt_image_masked, image[None, ...], r_final, outlier_prob, outlier_volume)[0]
+
+        final_results.append((score, overlap, pose, obj_idx, image, image_unmasked))
+
+    if len(final_results) > 0:
+        normalized_probabilites = jax3dp3.utils.normalize_log_scores(jnp.array([item[0] for item in final_results]))
+        best_overlap = final_results[0][1]
+        print('best_overlap:');print(best_overlap)
+        if best_overlap[0] / best_overlap[1] < 0.95  or  best_overlap[2] / best_overlap[3] < 0.95:
+            print("Overlap below necessary threshold")
+            normalized_probabilites = normalized_probabilites * 0.0
+    else:
+        normalized_probabilites = []
+
+    items = [(*data, prob) for (data,prob) in zip(final_results, normalized_probabilites)]
+    scores = np.array([item[0] for item in final_results])
+    final_results = [items[i] for i in np.argsort(-scores)]
+
+    # final_results = [(*data, prob) for (data,prob) in zip(final_results, normalized_probabilites)]
+    return final_results
+
+
 def multi_panel_c2f_viz(results:list, rgb, point_cloud_image, h, w, max_depth, model_names,title=None):
     if rgb.shape[-1] == 3:
         rgb_viz = jax3dp3.viz.get_rgb_image(rgb, 255.0)
@@ -134,8 +255,8 @@ def multi_panel_c2f_viz(results:list, rgb, point_cloud_image, h, w, max_depth, m
         overlap = (0,1,0,1)
         
     dst = jax3dp3.viz.multi_panel(
-        [rgb_viz_resized, gt_img_viz, *overlays],
-        labels=["RGB", "Depth Segment", *labels],
+        [rgb_viz_resized, gt_img_viz, *overlays] ,
+        labels=["RGB", "Depth Segment", *labels] ,
         bottom_text="{}\n Normalized Probabilites: {}\n Overlap: {}".format(
             jnp.array(scores), jnp.round(jnp.array(probabilities), decimals=4),
             "{:.4f} {:.4f}".format(overlap[0] / overlap[1], overlap[2] / overlap[3])
@@ -145,3 +266,4 @@ def multi_panel_c2f_viz(results:list, rgb, point_cloud_image, h, w, max_depth, m
     )
 
     return dst
+
