e16b8ce03c345ff65ec50306e0200a538b1c581a
Khaled Shehada <shehadak@mit.edu>
2023-02-03 19:00:16 +0000

    save cognitive experiments

--- experiments/cognitive-battery/cog_utils.py
+++ experiments/cognitive-battery/cog_utils.py
@@ -5,10 +5,11 @@ from scipy.spatial.transform import Rotation as R
 
 import json
 
+
 def get_object_transforms(obj_name, init_transform):
     with open("object_transforms.json") as f:
         object_transforms = json.load(f)
-        
+
     if obj_name not in object_transforms:
         return [init_transform]
 
@@ -17,17 +18,20 @@ def get_object_transforms(obj_name, init_transform):
         new_transform = init_transform.copy()
         object_transforms[obj_name]
         if "rot" in transform:
-            rot = R.from_euler('zyx', transform["rot"], degrees=True).as_matrix()
-            new_transform = new_transform.at[:3,:3].set(jnp.array(rot))
+            rot = R.from_euler("zyx", transform["rot"], degrees=True).as_matrix()
+            new_transform = new_transform.at[:3, :3].set(jnp.array(rot))
         if "scale" in transform:
             scale = jnp.array(transform["scale"])
-            new_transform = new_transform.at[:3,:3].set(new_transform[:3,:3] * scale)
+            new_transform = new_transform.at[:3, :3].set(new_transform[:3, :3] * scale)
         if "pos_delta" in transform:
-            t = jnp.array(transform["pos_delta"]).reshape(3,1)
-            delta_pose = jnp.vstack([jnp.hstack([jnp.eye(3), t]), jnp.array([0.0, 0.0, 0.0, 1.0])])
+            t = jnp.array(transform["pos_delta"]).reshape(3, 1)
+            delta_pose = jnp.vstack(
+                [jnp.hstack([jnp.eye(3), t]), jnp.array([0.0, 0.0, 0.0, 1.0])]
+            )
             new_transform = new_transform.dot(delta_pose)
         transforms.append(new_transform)
     return transforms
+
 
 def get_camera_intrinsics(width, height, fov):
     cx, cy = width / 2.0, height / 2.0
@@ -39,11 +43,12 @@ def get_camera_intrinsics(width, height, fov):
 
     return fx, fy, cx, cy
 
+
 def cluster_camera_coords(coords, eps=0.1, min_points_per_shape=5):
     # Given an array of depth coordinates [w, h, 3], finds clusters of points that belong to the same shape
-    
+
     flat_coords = coords.reshape(-1, 3)
-    clustering = DBSCAN(eps=eps, min_samples=min_points_per_shape, n_jobs=-1).fit(flat_coords)
+    clustering = DBSCAN(eps=eps, min_samples=min_points_per_shape, n_jobs=-1).fit(
+        flat_coords
+    )
     return clustering
-    
-    
