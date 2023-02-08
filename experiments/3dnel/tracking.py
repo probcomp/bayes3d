5c3cdeb3724cd8442d952517fe7dfa86e3c574c6
Nishad Gothoskar <nishad@mit.edu>
2023-02-08 15:38:49 +0000

    tracking

--- experiments/3dnel/tracking.py
+++ experiments/3dnel/tracking.py
@@ -32,7 +32,7 @@ from threednel.bop.data import RGBDImage
 from threednel.likelihood import ndl
 from threednel.likelihood.trace import Trace
 from threednel.utils.transform import depth_to_coords_in_camera
-from threednel.siren_jax import siren_jax_from_surfemb
+from threednel.surfemb.siren_jax import siren_jax_from_surfemb
 from threednel.utils.gcs import load_mesh
 
 
@@ -74,11 +74,7 @@ jax3dp3.viz.save_depth_image(obs_point_cloud_images[0,:,:,2], "depth.png", max=f
 images = jax3dp3.render_multiobject(poses, [0,1])
 jax3dp3.viz.save_depth_image(images[:,:,2], "depth2.png", max=far)
 
-deltas = jax3dp3.make_translation_grid_enumeration(
-    -50.0, -50.0, -50.0,
-    50.0, 50.0, 50.0,
-    5,5,5
-)
+
 r, outlier_prob, outlier_volume = 0.05, 0.01, 1**3
 
 # %%
@@ -113,6 +109,21 @@ voting.load_all_model_descriptors(model)
 #     jax3dp3.viz.get_depth_image(obs_point_cloud_images[t,:,:,2], max=far)
 # ]
 # ).save("data.png")
+
+
+t = 0
+rgb_img = rgb_images[t][:,:,:3]
+depth_img = obs_point_cloud_images[t,:,:,2]
+K = np.array([
+    [fx, 0.0, cx],
+    [0.0, fy, cy],
+    [0.0, 0.0, 1.0],
+])
+bop_obj_indices = np.array([2,3])
+
+test_img = RGBDImage(
+    rgb_img, depth_img, K, bop_obj_indices
+)
 
 offsets_ = []
 scales_ = []
@@ -150,24 +161,34 @@ infer_mlp = siren_jax_from_surfemb(model)
 from threednel.surfemb import utils
 
 
-deltas = jax3dp3.make_translation_grid_enumeration(
-    -50.0, -50.0, -50.0,
-    50.0, 50.0, 50.0,
-    5,5,5
-)
+
 r, outlier_prob, outlier_volume = 0.01, 0.01, 1**3
 
-variances = jnp.diag(jnp.array([600.0, 10.0, 0.001]))
-keys = jax.random.split(jax.random.PRNGKey(3), deltas.shape[0])
-drift_poses = jax.vmap(jax3dp3.distributions.gaussian_pose, in_axes=(0,None))(keys, variances)
-initial_pose_estimates = poses.copy()
-particles = jnp.tile(initial_pose_estimates[:, None, :,:], (1, deltas.shape[0], 1,1))
+
+from threednel.likelihood import ndl, ndl_fast
+outlier_scaling = 1 / 70000
+
+filter_shape = (5, 5)
+jax_ndl = ndl_fast.JAXNDL(
+    model=model,
+    n_objs=len(test_img.bop_obj_indices),
+    r=r,
+    outlier_prob=outlier_prob,
+    outlier_volume=outlier_volume,
+    outlier_scaling=outlier_scaling,
+    filter_shape=filter_shape,
+)
 
 all_particles = []
 viz_images = []
 
-r, outlier_prob, outlier_volume = 0.01, 0.01, 1**3
+variances = jnp.diag(jnp.array([600.0, 10.0, 0.001]))
 
+NUM_PARTICLES = 400
+keys = jax.random.split(jax.random.PRNGKey(3), NUM_PARTICLES)
+drift_poses = jax.vmap(jax3dp3.distributions.gaussian_pose, in_axes=(0,None))(keys, variances)
+initial_pose_estimates = poses.copy()
+particles = jnp.tile(initial_pose_estimates[:, None, :,:], (1, NUM_PARTICLES, 1,1))
 for t in range(obs_point_cloud_images.shape[0]):
     print(t)
     obj_idx = 1
@@ -204,10 +225,47 @@ for t in range(obs_point_cloud_images.shape[0]):
     scales_array = scales[segmentation_image_integer + 1]
     model_point_cloud_image_transformed = (model_point_cloud_image - offsets_array[...]) / scales_array[...,None]
 
-    model_descriptors = jax.vmap(infer_mlp, in_axes=(0,0))(
-        model_point_cloud_image_transformed.reshape(-1,3),
-        (segmentation_image_integer + 1) .reshape(-1,1)
-    )[:,0,0,0,0,:].reshape(segmentation_image_integer.shape[0],h,w,12)
+    model_descriptors_list = []
+    for i in range(int(particles.shape[1]/25)):
+        model_descriptors_list.append(infer_mlp(
+            model_point_cloud_image_transformed[25*i:25*i + 25],
+            segmentation_image_integer[25*i:25*i + 25],
+            jnp.array([2,3])
+        ))
+
+    model_descriptors = jnp.concatenate(
+        model_descriptors_list, axis=0)
+
+    # %%
+    jax_ndl.set_for_new_img(
+        data_xyz=data_xyz,
+        data_descriptors=data_descriptors,
+        log_normalizers=log_normalizers,
+        data_mask=data_mask,
+        bop_obj_indices=test_img.bop_obj_indices,
+        renderer=None,
+    )
+
+    p_background = (
+        outlier_prob / outlier_volume * outlier_scaling
+    )
+    p_foreground = (1.0 - outlier_prob) / model_mask.sum()
+
+    parallel_ndl = jax.vmap(ndl_fast.neural_descriptor_likelihood, in_axes=(None, None, None, 0, 0, 0, 0, None, None, None, None, None))
+    weights = parallel_ndl(
+        data_xyz,
+        data_descriptors,
+        log_normalizers,
+        images,
+        model_descriptors,
+        images[:,:,:,2] > 0.0,
+        segmentation_image_integer,
+        data_mask,
+        r,
+        p_background,
+        p_foreground,
+        filter_shape,
+    )
 
     # model_descriptors_visualizations = utils.get_model_descriptors_visualizations(
     #     np.array(model_descriptors[0]),
@@ -222,9 +280,14 @@ for t in range(obs_point_cloud_images.shape[0]):
     # plt.savefig("descriptors_new.png")
 
     ###### @Stannis replace this with the new likelihood.
-    weights = jax3dp3.threedp3_likelihood_parallel_jit(
-        obs_point_cloud_images[t] / 1000.0, images / 1000.0, r, outlier_prob, outlier_volume
-    )
+    # weights = jax3dp3.threedp3_likelihood_parallel_jit(
+    #     obs_point_cloud_images[t] / 1000.0, images / 1000.0, r, outlier_prob, outlier_volume
+    # )
+
+    # weights = jax3dp3.threedp3_likelihood_parallel_jit(
+    #     obs_point_cloud_images[t] / 1000.0, images / 1000.0, r, outlier_prob, outlier_volume
+    # )
+
     parent_idxs = jax.random.categorical(keys[0], weights, shape=weights.shape)
     particles = particles[:,parent_idxs]
     keys = jax.random.split(keys[0], weights.shape[0])
