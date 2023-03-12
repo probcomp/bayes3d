4b04026d843c018cf8ee8a4b0fbebdbbf1b501a3
Nishad Gothoskar <nishad@mit.edu>
2023-03-12 03:19:45 +0000

    phsyics working

--- jax3dp3/likelihood.py
+++ jax3dp3/likelihood.py
@@ -50,25 +50,34 @@ threedp3_likelihood_with_r_parallel_jit = jax.jit(
 )
 
 
-def threedp3_likelihood_get_counts(
+
+import functools
+from functools import partial
+@functools.partial(
+    jnp.vectorize,
+    signature='(m)->()',
+    excluded=(1,2,3,4,),
+)
+def get_counts(
+    ij,
+    data_xyz: jnp.ndarray,
+    model_xyz: jnp.ndarray,
+    filter_size: int,
+    r,
+):
+    dists = data_xyz[ij[0], ij[1], :3] - jax.lax.dynamic_slice(model_xyz, (ij[0], ij[1], 0), (2*filter_size + 1, 2*filter_size + 1, 3))
+    probs = (jnp.linalg.norm(dists, axis=-1) < r).sum()
+    return probs
+
+def threedp3_counts(
     obs_xyz: jnp.ndarray,
     rendered_xyz: jnp.ndarray,
     r,
 ):
     filter_size = FILTER_SIZE
-    obs_mask = obs_xyz[:,:,2] > 0.0
-    rendered_mask = rendered_xyz[:,:,2] > 0.0
-    
+    num_latent_points = obs_xyz.shape[1] * obs_xyz.shape[0]
     rendered_xyz_padded = jax.lax.pad(rendered_xyz,  -100.0, ((filter_size,filter_size,0,),(filter_size,filter_size,0,),(0,0,0,)))
     jj, ii = jnp.meshgrid(jnp.arange(obs_xyz.shape[1]), jnp.arange(obs_xyz.shape[0]))
     indices = jnp.stack([ii,jj],axis=-1)
-    counts_obs = count_ii_jj(indices, obs_xyz, rendered_xyz_padded, r, filter_size)
-    
-    obs_xyz_padded = jax.lax.pad(obs_xyz,  -100.0, ((filter_size,filter_size,0,),(filter_size,filter_size,0,),(0,0,0,)))
-    jj, ii = jnp.meshgrid(jnp.arange(rendered_xyz.shape[1]), jnp.arange(rendered_xyz.shape[0]))
-    indices = jnp.stack([ii,jj],axis=-1)
-    counts_rendered = count_ii_jj(indices, rendered_xyz, obs_xyz_padded, r, filter_size)
-    return jnp.array([
-        (obs_mask * (counts_obs > 0)).sum(), obs_mask.sum(), (rendered_mask * (counts_rendered > 0)).sum(), rendered_mask.sum()
-    ])
-
+    counts = get_counts(indices, obs_xyz, rendered_xyz_padded, filter_size, r)
+    return counts
