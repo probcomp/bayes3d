edffa891b20c2c31c89243873527c3c266d9fd3f
Nishad Gothoskar <nishadg@mit.edu>
2023-04-10 14:19:41 +0000

    nishad

--- experiments/c2f_benchmark/c2f_nishad_single.py
+++ experiments/c2f_benchmark/c2f_nishad_single.py
@@ -17,11 +17,12 @@ data = np.load(DATASET_FILE,allow_pickle=True)
 rgbds = data["rgbds"]
 poses = data["poses"]
 id = data["id"].item()
-IMG_NUMBER = 0
+
+IMG_NUMBER = 1
 rgbd = rgbds[IMG_NUMBER]
 gt_pose = poses[IMG_NUMBER]
 original_intrinsics = rgbd.intrinsics
-intrinsics = j.scale_camera_parameters(original_intrinsics, 0.25)
+intrinsics = j.scale_camera_parameters(original_intrinsics, 0.1)
 
 
 model_dir = os.path.join(j.utils.get_assets_dir(), "bop/ycbv/models")
@@ -34,43 +35,36 @@ depth_scaled = j.utils.resize(depth, intrinsics.height, intrinsics.width)
 point_cloud_image = j.t3d.unproject_depth(depth_scaled, intrinsics)
 
 j.meshcat.setup_visualizer()
+
+# configure c2f
+grid_widths = [0.1, 0.05, 0.03, 0.01, 0.01, 0.01]
+rot_angle_widths = [jnp.pi, jnp.pi, jnp.pi, jnp.pi, jnp.pi/5, jnp.pi/5]
+sphere_angle_widths = [jnp.pi, jnp.pi/2, jnp.pi/4, jnp.pi/4, jnp.pi/5, jnp.pi/5]
+grid_params =  [(3,3,3,75*5,15), (3,3,3,75*3,21),(3,3,3,55,45),(3,3,3,55,45), (3,3,3,45,45), (3,3,3,45,45)]  # (num_x, num_y, num_z, num_fib_sphere, num_planar_angle)
+
+scheds = j.c2f.make_schedules(
+    grid_widths=grid_widths, 
+    angle_widths=rot_angle_widths, 
+    grid_params=grid_params, 
+    full_pose=True, 
+    sphere_angle_widths=sphere_angle_widths
+)
+
+
 j.meshcat.show_cloud("1", point_cloud_image.reshape(-1,3))
-
-
-# T_HALF_WIDTH = 0.1
-# translation_grid = j.enumerations.make_translation_grid_enumeration(-T_HALF_WIDTH,-T_HALF_WIDTH,-T_HALF_WIDTH,T_HALF_WIDTH,T_HALF_WIDTH,T_HALF_WIDTH,10,10,10)
-
-# rotation_grid = j.enumerations.get_rotation_proposals(200,20)
-
-# angles = jnp.linspace(0.0, 2*jnp.pi, 301)
-# rotations_z = jax.vmap(t3d.transform_from_axis_angle,in_axes=(None, 0))(jnp.array([0.0,0.0, 1.0]), angles)
-# rotations_y = jax.vmap(t3d.transform_from_axis_angle,in_axes=(None, 0))(jnp.array([0.0,1.0, 0.0]), angles)
-# rotations_x = jax.vmap(t3d.transform_from_axis_angle,in_axes=(None, 0))(jnp.array([1.0,0.0, 0.0]), angles)
-
-# all_translation_grids = [translation_grid]
-# all_rotation_grids = [rotations_z, rotations_y, rotations_x, rotation_grid]
-# all_grids = [all_translation_grids, all_rotation_grids]
-
 
 center = jnp.mean(point_cloud_image[point_cloud_image[:,:,2]< intrinsics.far],axis=0)
 pose_estimate = j.t3d.transform_from_pos(center)
 best_weight = -jnp.inf
 
-
-for it in tqdm(range(50)):
-    translation_iter = (it % 2) ==0
-
-    for grid in all_grids[it % 2]:
-        if translation_iter:
-            pose_proposals = jnp.einsum('aij,jk->aik', grid, pose_estimate)
-        else:
-            pose_proposals = jnp.einsum('ij,ajk->aik', pose_estimate, grid)
-
+for deltas in scheds:
+    for batch in jnp.array_split(deltas, deltas.shape[0] // 2000):
+        pose_proposals = jnp.einsum('ij,ajk->aik', pose_estimate, batch)
 
         rendered_depth = renderer.render_parallel(pose_proposals, 0)[...,2]
         rendered_point_cloud_images = j.t3d.unproject_depth_vmap_jit(rendered_depth, intrinsics)
 
-        R_SWEEP = jnp.array([0.05])
+        R_SWEEP = jnp.array([0.02])
         OUTLIER_PROB=0.1
         OUTLIER_VOLUME=1.0
         weights = j.threedp3_likelihood_with_r_parallel_jit(
@@ -80,20 +74,20 @@ for it in tqdm(range(50)):
             pose_estimate = pose_proposals[weights.argmax()]
             best_weight = weights.max()
     print(best_weight)
+    j.meshcat.show_cloud("2", renderer.render_single_object(pose_estimate, 0)[...,:3].reshape(-1,3), color=j.RED)
 
 j.meshcat.show_cloud("2", renderer.render_single_object(pose_estimate, 0)[...,:3].reshape(-1,3), color=j.RED)
-print(weights.max())
 
 
 
-pose_proposals = jnp.array([gt_pose, gt_pose])
-rendered_depth = renderer.render_parallel(pose_proposals, 0)[...,2]
-rendered_point_cloud_images = j.t3d.unproject_depth_vmap_jit(rendered_depth, intrinsics)
+# pose_proposals = jnp.array([gt_pose, gt_pose])
+# rendered_depth = renderer.render_parallel(pose_proposals, 0)[...,2]
+# rendered_point_cloud_images = j.t3d.unproject_depth_vmap_jit(rendered_depth, intrinsics)
 
-weights = j.threedp3_likelihood_with_r_parallel_jit(
-    point_cloud_image, rendered_point_cloud_images, R_SWEEP, OUTLIER_PROB, OUTLIER_VOLUME
-)
-print(weights.max())
+# weights = j.threedp3_likelihood_with_r_parallel_jit(
+#     point_cloud_image, rendered_point_cloud_images, R_SWEEP, OUTLIER_PROB, OUTLIER_VOLUME
+# )
+# print(weights.max())
 # j.meshcat.show_cloud("2", renderer.render_single_object(jnp.array(gt_pose), 0)[...,:3].reshape(-1,3), color=j.RED)
 
 from IPython import embed; embed()
