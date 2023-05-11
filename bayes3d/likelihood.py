4f6455727aef08b8177f71f7719eab544c61b32b
Nishad Gothoskar <nishadg@mit.edu>
2023-05-11 21:18:38 +0000

    likelihood

--- bayes3d/likelihood.py
+++ bayes3d/likelihood.py
@@ -109,33 +109,35 @@ threedp3_likelihood_multi_r_full_hierarchical_bayes_jit = jax.jit(jax.vmap(jax.v
 @functools.partial(
     jnp.vectorize,
     signature='(m)->()',
-    excluded=(1,2,3,4,5,),
+    excluded=(1,2,3,4,5,6,7,),
 )
 def gausssian_mixture_vectorize(
     ij,
     observed_xyz: jnp.ndarray,
     rendered_xyz_padded: jnp.ndarray,
-    r,
-    num_mixture_components, 
+    variance,
+    num_mixture_components,
+    outlier_prob: float,
+    outlier_volume: float,
     filter_size: int,
 ):
     distances = (
         observed_xyz[ij[0], ij[1], :3] - 
         jax.lax.dynamic_slice(rendered_xyz_padded, (ij[0], ij[1], 0), (2*filter_size + 1, 2*filter_size + 1, 3))
     )
-    probability = (
+    probability = jax.scipy.special.logsumexp(
         jax.scipy.stats.norm.pdf(
             distances,
             loc=0.0,
-            scale=jnp.sqrt(r)
-        ).prod(-1) / num_mixture_components
-    ).sum()
-    return probability
+            scale=jnp.sqrt(variance)
+        ).sum(-1) - jnp.log(num_mixture_components)
+    )
+    return jnp.logaddexp(probability + jnp.log(1.0 - outlier_prob), jnp.log(outlier_prob) - jnp.log(outlier_volume))
 
 def threedp3_likelihood_per_pixel(
     observed_xyz: jnp.ndarray,
     rendered_xyz: jnp.ndarray,
-    r,
+    variance,
     outlier_prob,
     outlier_volume,
     filter_size
@@ -145,26 +147,25 @@ def threedp3_likelihood_per_pixel(
     rendered_xyz_padded = jax.lax.pad(rendered_xyz,  -100.0, ((filter_size,filter_size,0,),(filter_size,filter_size,0,),(0,0,0,)))
     jj, ii = jnp.meshgrid(jnp.arange(observed_xyz.shape[1]), jnp.arange(observed_xyz.shape[0]))
     indices = jnp.stack([ii,jj],axis=-1)
-    probabilities = gausssian_mixture_vectorize(
+    log_probabilities = gausssian_mixture_vectorize(
         indices, observed_xyz,
         rendered_xyz_padded,
-        r, num_mixture_components, filter_size
+        variance, num_mixture_components, outlier_prob, outlier_volume, filter_size
     )
-    probabilities_with_outlier_model = probabilities * (1.0 - outlier_prob)  + outlier_prob / outlier_volume
-    return jnp.log(probabilities_with_outlier_model)
+    return log_probabilities
 
 threedp3_likelihood_per_pixel_jit = jax.jit(threedp3_likelihood_per_pixel, static_argnames=('filter_size',))
 
 def threedp3_likelihood(
     observed_xyz: jnp.ndarray,
     rendered_xyz: jnp.ndarray,
-    r,
+    variance,
     outlier_prob,
     outlier_volume,
     filter_size
 ):
     log_probabilities_per_pixel = threedp3_likelihood_per_pixel(
-        observed_xyz, rendered_xyz, r,
+        observed_xyz, rendered_xyz, variance,
         outlier_prob, outlier_volume, filter_size
     )
     return log_probabilities_per_pixel.sum()
--- test/test_likelihood.py
+++ test/test_likelihood.py
@@ -11,11 +11,13 @@ r_array = jnp.linspace(0.01, 0.1,100)
 outlier_array = jnp.linspace(0.01, 0.1,100)
 
 print(j.threedp3_likelihood_multi_r_jit(
-    obs, rendered, rendered_seg, jnp.array([0.1, 0.2]), 0.1, 0.1, 3
+    obs, rendered, rendered_seg, jnp.array([0.6, 0.6]), 0.2, 0.1, 3
 ))
 
-print(j.threedp3_likelihood_jit(
-    obs, rendered, 0.6, 0.2, 0.1, 3
-))
+for i in range(10):
+    print(i)
+    print(j.threedp3_likelihood_jit(
+        obs, rendered, 0.6, 0.2, 0.1, i
+    ))
 
 from IPython import embed; embed()
