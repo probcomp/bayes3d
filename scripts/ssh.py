a4a96366c72763599d4649df78f6033ed39bc543
Nishad Gothoskar <nishadg@mit.edu>
2023-09-20 16:42:52 +0000

    ssh

--- scripts/ssh.py
+++ scripts/ssh.py
@@ -6,9 +6,12 @@ class SSHSender:
         self.ssh = paramiko.SSHClient()
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.ssh.connect(hostname, username=username, key_filename=ssh_key_path)
-        self.scp = SCPClient(ssh.get_transport())
+        self.scp = SCPClient(self.ssh.get_transport())
 
         self.result_directory = result_directory
 
     def send_file(self, filename):
         self.scp.put(filename, remote_path=self.result_directory)
+
+sender = SSHSender('34.123.143.56', 'nishadgothoskar', '/Users/nishadgothoskar/.ssh/id_ed25519.pub', ".")
+sender.send_file("demo.gif")
