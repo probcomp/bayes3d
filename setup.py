c25ab41335a474dcb5bc5bc20e801baaf9bfd68e
Sam Ritchie <sam@mentat.org>
2023-10-05 15:24:39 -0600

    build: flesh out setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,36 @@
-import setuptools
+#!/usr/bin/python
 
-NAME = 'bayes3d'
+from setuptools import find_namespace_packages, setup
+
+
+def readme():
+    try:
+        with open('README.md') as f:
+            return f.read()
+    except Exception:
+        return None
+
+
+REQUIRED_PACKAGES = [
+    'scikit-learn', 'wheel', 'ipython', 'jupyter', 'numpy', 'matplotlib',
+    'pillow', 'opencv-python', 'open3d', 'graphviz', 'distinctipy', 'trimesh',
+    'ninja', 'pyransac3d', 'meshcat', 'h5py', 'gdown', 'pytest', 'zmq',
+    'plyfile', 'tqdm', 'jupyterlab', 'imageio', 'timm', 'joblib', 'pdoc3',
+    'addict', 'tensorflow-probability', 'flax'
+]
+
 VERSION = '0.0.1'
-if __name__ == '__main__':
-    setuptools.setup(
-        name=NAME,
-        version=VERSION,
-        packages=setuptools.find_namespace_packages(include=['bayes3d.*']),
-    )
+
+setup(name='bayes3d',
+      version=VERSION,
+      description='Docker-based job runner for AI research.',
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+      python_requires='>=3.10.0',
+      author='Nishad Gothoskar',
+      author_email='nishadg@mit.edu',
+      url='https://github.com/probcomp/bayes3d',
+      license='Apache-2.0',
+      packages=find_namespace_packages(include=['bayes3d.*']),
+      install_requires=REQUIRED_PACKAGES,
+      include_package_data=True)
