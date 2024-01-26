probcomp/bayes3d

commit a3807d092161924a88ba787f8a9b9ed444d67c38

Add pre-commit hooks, Ruff formatting and linting (#42)

bayes3d/__init__.py
@@ -1,24 +1,12 @@
 """
 .. include:: ./documentation.md
 """
-from .transforms_3d import *
+
+from .camera import *
+from .likelihood import *
 from .renderer import *
 from .rgbd import *
-from .likelihood import *
-from .camera import *
+from .transforms_3d import *
 from .viz import *
-from . import utils
-from . import distributions
-from . import scene_graph
-from . import colmap
-
-try:
-    import genjax
-    from .genjax import *
-except ImportError as e:
-    print("GenJAX not installed. Importing bayes3d without genjax dependencies.")
-    print(e)
-
-
 
 RENDERER = None

pyproject.toml
@@ -1,0 +1,44 @@
+[tool.ruff]
+exclude = [
+    ".bzr",
+    ".direnv",
+    ".eggs",
+    ".git",
+    ".git-rewrite",
+    ".hg",
+    ".mypy_cache",
+    ".nox",
+    ".pants.d",
+    ".pytype",
+    ".ruff_cache",
+    ".svn",
+    ".tox",
+    ".venv",
+    "__pypackages__",
+    "_build",
+    "buck-out",
+    "build",
+    "dist",
+    "node_modules",
+    "venv"
+]
+# extend-include = ["*.ipynb"]
+line-length = 88
+indent-width = 4
+
+[tool.ruff.lint]
+exclude = ["bayes3d/_mkl/*.py"]
+extend-select = ["I"]
+select = ["E4", "E7", "E9", "F"]
+
+# F403 disables errors from `*` imports, which we currently use heavily.
+ignore = ["F403"]
+fixable = ["ALL"]
+unfixable = []
+dummy-variable-rgx = "^(_+|(_+[a-zA-Z0-9_]*[a-zA-Z0-9]+?))$"
+
+[tool.ruff.format]
+quote-style = "double"
+indent-style = "space"
+skip-magic-trailing-comma = false
+line-ending = "auto"

setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
-NAME = 'bayes3d'
-VERSION = '0.0.1'
-if __name__ == '__main__':
+NAME = "bayes3d"
+VERSION = "0.0.1"
+if __name__ == "__main__":
     setuptools.setup(
         name=NAME,
         version=VERSION,
-        packages=setuptools.find_namespace_packages(include=['bayes3d.*']),
+        packages=setuptools.find_namespace_packages(include=["bayes3d.*"]),
     )

bayes3d/rendering/nvdiffrast/__init__.py
@@ -6,4 +6,4 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
-__version__ = '0.3.0'
+__version__ = "0.3.0"

bayes3d/rendering/nvdiffrast/common/__init__.py
@@ -6,5 +6,6 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
-from .ops import RasterizeGLContext, get_log_level, set_log_level, _get_plugin
+from .ops import RasterizeGLContext, _get_plugin, get_log_level, set_log_level
+
 __all__ = ["RasterizeGLContext", "get_log_level", "set_log_level", "_get_plugin"]

bayes3d/rendering/nvdiffrast_jax/__init__.py
@@ -6,4 +6,4 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
-__version__ = '0.3.0'
+__version__ = "0.3.0"

commit d189c5da4ee032e33ba006597a5748da830cd1f7

Move version & package discovery to pyproject.toml
Also fix the import error introduced in #42

bayes3d/__init__.py
@@ -2,6 +2,7 @@
 .. include:: ./documentation.md
 """
 
+from . import colmap, distributions, scene_graph, utils
 from .camera import *
 from .likelihood import *
 from .renderer import *
@@ -10,3 +11,5 @@ from .transforms_3d import *
 from .viz import *
 
 RENDERER = None
+
+__all__ = ["colmap", "distributions", "scene_graph", "utils"]

pyproject.toml
@@ -4,6 +4,7 @@ build-backend = "setuptools.build_meta"
 
 [project]
 name = "bayes3d"
+version = "0.0.1"
 authors = [
     {name = "Nishad Gothoskar", email = "nishadg@mit.edu"},
 ]
@@ -24,7 +25,6 @@ classifiers = [
     "Programming Language :: Python :: 3.11",
     "Programming Language :: Python :: 3.12"
 ]
-dynamic = ["version"]
 
 [tool.ruff]
 exclude = [
@@ -70,3 +70,8 @@ quote-style = "double"
 indent-style = "space"
 skip-magic-trailing-comma = false
 line-ending = "auto"
+
+
+[tool.setuptools.packages.find]
+include = ["bayes3d"]
+namespaces = false

setup.py
@@ -1,10 +1,0 @@
-import setuptools
-
-NAME = "bayes3d"
-VERSION = "0.0.1"
-if __name__ == "__main__":
-    setuptools.setup(
-        name=NAME,
-        version=VERSION,
-        packages=setuptools.find_namespace_packages(include=["bayes3d.*"]),
-    )