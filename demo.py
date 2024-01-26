3c199c36eb1a20b4d19019be437e2fedb0b67e3d
Nishad Gothoskar <nishad@mit.edu>
2024-01-26 06:08:18 +0000

    Save

--- demo.py
+++ demo.py
@@ -7,6 +7,7 @@ from IPython import embed
 from scipy.spatial.transform import Rotation as R
 
 import bayes3d as b
+from bayes3d import distributions
 
 # Can be helpful for debugging:
 # jax.config.update('jax_enable_checks', True)
